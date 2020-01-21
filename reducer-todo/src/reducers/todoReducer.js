9fcd63c910558ce2a1f8113b69de3aafa2c0c3cd
Amber Mohler <ambermohler@iMac.local>
2020-01-21 17:47:16 -0500

    completed step 1

---
+++ reducer-todo/src/reducers/todoReducer.js
@@ -0,0 +1,41 @@
+
+const data = [
+    {
+        item: 'Unpack boxes',
+        completed: false,
+        id: 8594
+    },
+
+    {
+        item: 'Walk the dog',
+        completed: false,
+        id: 7546
+    },
+
+    {
+        item: 'Make dinner',
+        completed: false,
+        id: 9876
+    },
+
+    {
+        item: 'Do laundry',
+        completed: false,
+        id: 2465
+    },
+
+    {
+        item: 'Do dishes',
+        completed: false,
+        id: 8587
+    }
+]
+
+export const initialState = {
+    item: data
+}
+
+export const todoReducer = (state, action) => {
+
+    return state;
+}
